d99ce991675ceda1496b00e8170a2c21d81926c8
Conner Pearce <metalichking@outlook.com>
2020-03-17 13:17:57 +1300

    Add project files.

---
+++ GameCatalogueApp/GameCatalogueApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using GameCatalogueApp.Pages.Home;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GameCatalogueApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
