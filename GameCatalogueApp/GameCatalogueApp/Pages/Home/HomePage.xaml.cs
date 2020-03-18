78335e5ec5ee93da301dd044e127101accfb44ca
Conner Pearce <metalichking@outlook.com>
2020-03-18 13:01:42 +1300

    Added Login popup Created Registration page

--- GameCatalogueApp/GameCatalogueApp/Pages/Home/HomePage.xaml.cs
+++ GameCatalogueApp/GameCatalogueApp/Pages/Home/HomePage.xaml.cs
@@ -16,5 +16,26 @@ namespace GameCatalogueApp.Pages.Home
         {
             InitializeComponent();
         }
+
+        /// LOGIN FUNCTIONS ///
+
+        //LOGIN BUTTON
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            // Shows the popup while making other content not visible
+            // An Issue came up where things would overlap which could not be fixed without using custom renderers
+            // Or custom Programming (Less generic program)
+            popupLoginView.IsVisible = true;
+            lblWelcomeMessage.IsVisible = false;
+            txtSearchBar.IsVisible = false;
+        }
+
+        // LOGIN POP UP
+        // The Registration label is clickable
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            // Navigate to registration page
+            await Navigation.PushAsync(new Registration.Registration());
+        }
     }
 }
