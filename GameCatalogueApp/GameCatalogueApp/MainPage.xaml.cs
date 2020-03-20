058c2ec02a10deaa65fcb47dbf05c5bac694387d
Conner Pearce <metalichking@outlook.com>
2020-03-21 09:55:30 +1300

    Moved around code to clean up

--- GameCatalogueApp/GameCatalogueApp/MainPage.xaml.cs
+++ GameCatalogueApp/GameCatalogueApp/MainPage.xaml.cs
@@ -11,11 +11,16 @@ namespace GameCatalogueApp
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : NavigationPage
+    public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
